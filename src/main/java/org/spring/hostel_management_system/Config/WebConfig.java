e9fa27ab9f323685f3d40abc943a00fa9f0a6242
Devina Agrawal <devinaagrawal2005@gmail.com>
2026-06-09 13:48:10 +0530

    new commit

--- src/main/java/org/spring/hostel_management_system/Config/WebConfig.java
+++ src/main/java/org/spring/hostel_management_system/Config/WebConfig.java
@@ -18,7 +18,8 @@ public class WebConfig {
 
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "http://localhost:3000"
+                "http://localhost:3000",
+                "https://hostel-management-system-frontend-two.vercel.app"
         ));
 
         config.setAllowedMethods(List.of(
